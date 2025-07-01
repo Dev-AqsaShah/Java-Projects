11fb811573a9d2108925ff0def594f5fc7782e23
aqsashah <aqsanor665@gmail.com>
2025-07-01 16:17:08 +0500

    output methods done

--- firstcode.java
+++ firstcode.java
@@ -1,5 +1,45 @@
-class firstcode {
+// class firstcode {
+//     public static void main(String args[]) {
+//         System.out.print("Hello World");
+//     }
+// }
+
+// output
+
+// class firstcode {
+//     public static void main(String args[]) {
+//         System.out.println("hello from java");
+//         System.out.print("Hello from Java\n");
+//         System.out.print("Hello from\nJava");
+//     }
+// }   
+
+
+//  class firstcode {
+//     public static void main(String args[]) {
+//         System.out.print("Hello World");
+//     }
+// }
+
+
+ class firstcode {
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
     }
 }
+
+
+// Quiz
+
+// *
+// **
+
+// Options:
+// a. System.out.println("*\n**");
+// b. System.out.println("**\n*");
+// c. System.out.println("***\n");
+
+
