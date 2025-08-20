72650fc1c46c52518f99544479fbbc6734de1464
aqsashah <aqsanor665@gmail.com>
2025-08-20 21:18:11 +0500

    Update oops.java

--- oops.java
+++ oops.java
@@ -5,6 +5,10 @@ class Pen {
     public void write() {
         System.out.println("writing something");
     }
+
+    public void printColor() {
+        System.out.println(this.color);
+    }
 }
 
 
@@ -14,6 +18,14 @@ public class oops {
         pen1.color = "blue";
         pen1.type = "gel";
 
-        pen1.write();
+        Pen pen2 = new Pen();
+        pen2.color = "black";
+        pen2.type = "ballpoint";
+
+        pen1.printColor();
+        pen2.printColor();
+
     }
 }
+
+
