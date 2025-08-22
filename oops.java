2949031a126582a9e2bf4e18d63c1e9c54e61933
aqsashah <aqsanor665@gmail.com>
2025-08-22 21:29:19 +0500

    -

--- oops.java
+++ oops.java
@@ -12,18 +12,64 @@ class Pen {
 }
 
 
+class Student {
+    String name;
+    int age; 
+
+    public void printInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+
+    // normal constructor
+    Student() {
+        System.out.println("constructor called");
+    }
+
+
+
+// parameter constructor
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+// copy constructor
+    Student(Student s2){
+        this.name = s2.name;
+        this.age = s2.age;
+    }
+}
+
+
+
+
+
+
+
 public class oops {
     public static void main(String aegs[]) {
-        Pen pen1 = new Pen();
-        pen1.color = "blue";
-        pen1.type = "gel";
+        Student s1 = new Student("aqsa", 24);
+        s1.name = "aqsa";
+        s1.age = 22;
 
-        Pen pen2 = new Pen();
-        pen2.color = "black";
-        pen2.type = "ballpoint";
+        Student s2 = new Student(s1);
+        s2.printInfo();
+        s1.printInfo();
 
-        pen1.printColor();
-        pen2.printColor();
+
+
+        // Pen pen1 = new Pen();
+        // pen1.color = "blue";
+        // pen1.type = "gel";
+
+        // Pen pen2 = new Pen();
+        // pen2.color = "black";
+        // pen2.type = "ballpoint";
+
+        // pen1.printColor();
+        // pen2.printColor();
 
     }
 }
