396055f34b0afb39425fc6a749f49303796031dd
aqsashah <aqsanor665@gmail.com>
2025-08-24 20:22:22 +0500

    Update oops.java

--- oops.java
+++ oops.java
@@ -93,3 +93,48 @@ class Student {
         System.out.println(name + " " + age);
     }
 }
+
+
+
+//  Inheritance 
+
+class Shape {
+    public void area() {
+        System.out.println("displays area");
+    }
+}
+
+class Triangle extends Shape {
+    public void area(int l, int h) {
+        System.out.println(1/2*l*h);
+    }
+
+}
+
+class Circle extends Shape {
+    public void area(int r) {
+        System.out.println((3.14)*r*r);
+    }
+}
+
+// hybrid
+
+
+
+class EquilateralTriangle extends Traingle {
+    public void area(int l, int h) {
+        System.out.println(1/2*l*h);
+    }
+}
+
+
+public class oops {
+    public static void main(String args[]) {
+        Triangle t1 = new Triangle();
+        t1.color ="red";
+
+    }
+}
+
+
+// pakages
