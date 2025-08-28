dfddb318d1f821b27f6c16f1bcc518a8da769669
aqsashah <aqsanor665@gmail.com>
2025-08-28 11:30:44 +0500

    Update oops.java

--- oops.java
+++ oops.java
@@ -184,3 +184,24 @@ public class oops {
     }
     
 }
+
+
+// interfaces abstraction 
+
+interface Animal {
+    public void walk();
+}
+
+class Horse implements Animal {
+
+    public void walk() {
+        System.out.println("walks on 4 legs");
+    }
+}
+
+public class oops {
+    public static void main(String args[]) {
+        Horse horse = new Horse();
+        horse.walk();
+    }
+}
