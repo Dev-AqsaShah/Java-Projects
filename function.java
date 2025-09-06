a7007d27915e40f26725490c5b64284c2b0b5c64
aqsashah <aqsanor665@gmail.com>
2025-09-06 15:28:29 +0500

    Update function.java

--- function.java
+++ function.java
@@ -1,5 +1,13 @@
 //  main keyword 1. public 2. static 
 
 public class function {
-    
+    public static void printMyName(String name) {
+        System.out.println(name);
+        return;
+
+
+    }
+    public static void main(String args[]) {
+        
+    }
 }
