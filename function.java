7fba3bab472ced61bcfd339ca4c63db600c2728f
aqsashah <aqsanor665@gmail.com>
2025-09-07 21:24:45 +0500

    Update function.java

--- function.java
+++ function.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 //  main keyword 1. public 2. static 
 
 public class function {
@@ -8,6 +10,26 @@ public class function {
 
     }
     public static void main(String args[]) {
-        
+
     }
 }
+
+
+// question 1 
+
+public class Function {
+    public static int calculation(int a, int b) {
+        int sum = 0 + b;
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = calculation(a, b);
+        System.out.println("Sum of 2 number is : "+ sum);
+    }
+}
+
