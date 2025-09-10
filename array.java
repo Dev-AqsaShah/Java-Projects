eaaa9efed1f3df8f094c068e383e238c358bd1af
aqsashah <aqsanor665@gmail.com>
2025-09-10 22:08:42 +0500

    Update array.java

--- array.java
+++ array.java
@@ -8,8 +8,12 @@ public class array {
         marks[0] = 97;
         marks[1] = 98;
         marks[2] = 95;
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        for(int i=0; i<3; i++) {
+            System.out.println(marks[i]);
+        }
     }
 }
