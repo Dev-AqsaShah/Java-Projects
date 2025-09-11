65bc18dba103a39edbaf58a7871cd56641a31298
aqsashah <aqsanor665@gmail.com>
2025-09-11 12:26:17 +0500

    strings

---
+++ strings.java
@@ -0,0 +1,26 @@
+// non primitive data types
+// 
+
+import java.util.Scanner;
+
+public class strings {
+    public static void main(String args[]) {
+        // String declaration
+        // String name = "aqsa";
+        // String fulname = "aqsa shah";
+        // String sentence = "im aqsa shah here. ";
+
+        // Scanner sc = new Scanner(System.in);
+        //     String name = sc.next();
+        //     System.out.println("your name is : "+ name);
+
+        // functions use string
+        // 1. concatination
+        String firstName = "aqsa";
+        String lastName = "shah";
+        String fulName = firstName + lastName;
+        System.out.println(fulName);
+    }
+}
+
+
