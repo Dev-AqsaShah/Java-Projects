c8c4d9d3ffb00fbc9a5200cc88ac9a8423cd5ca4
aqsashah <aqsanor665@gmail.com>
2025-09-13 22:23:34 +0500

    Update strings.java

--- strings.java
+++ strings.java
@@ -33,7 +33,17 @@ public class strings {
         String name1 = "Tony";
         String name2 = "Tony";
 
-        if()
+        if(name1.compareTo(name2) == 0) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+
+        if(new String("Tony") == new String("Tony")) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are equal");
+        }
     }
 }
 
