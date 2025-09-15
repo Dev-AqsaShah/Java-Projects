9a779d0a600c8396e209df2285adf6fcb8f8affc
aqsashah <aqsanor665@gmail.com>
2025-09-15 22:02:25 +0500

    .

--- operators.java
+++ operators.java
@@ -3,4 +3,29 @@ public class operators {
     
 }
 
-// symbols that tell compiler to perform some operation
+// symbols that tell compiler to perform some operation.
+// arithmetic operators 
+// binary -> + , = , * , / , % 
+// unary -> ++ --
+
+
+// tow types of increament operateor 
+// 1, pre increament
+// 2, post increament
+
+// RELATIONAL operators
+// ==
+// !=
+// >
+// <
+// >=
+// <=
+
+// LOGICAL operatoers
+// &&
+// || or
+// ! not
+
+// binnary number system
+// octal  base 8
+// hexadecimal base 16
