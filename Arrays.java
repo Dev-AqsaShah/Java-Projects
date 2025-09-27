f04c9cefeb16fb9a539756149c5234ae2cb62341
AqsaShah <159179853+Dev-AqsaShah@users.noreply.github.com>
2025-09-27 09:03:46 -0700

    .

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+
+// defining an array 2
+// type[] arrayName = {1,2,3,4,5,6};
+
+
+public class array {
+    public static void main(String args[]) {
+        int marks[] = {97, 98, 95};
+           System.out.println(marks[i]);
+    }
+}
