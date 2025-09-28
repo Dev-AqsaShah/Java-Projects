0a1ca84161da5536329c1e59e8424d43188e2ad2
AqsaShah <159179853+Dev-AqsaShah@users.noreply.github.com>
2025-09-28 08:14:30 -0700

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 // defining an array 2
 // type[] arrayName = {1,2,3,4,5,6};
 
@@ -9,3 +9,22 @@ public class array {
            System.out.println(marks[i]);
     }
 }
+
+
+public class Arrays {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+
+        for(int i=0; i<size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<size; i++) {
+            System.out.println(numbers[i])
+        }
+    }
+}
+
+
